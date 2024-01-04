6d10973ec223227743b70e61011a00e5f2c5dd84
Efimish <efim.ishenin@gmail.com>
2024-01-05 00:20:51 +0700

    started doing something

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,20 @@
-<h1>My Profile should be here</h1>
+<svelte:head>
+    <title>My Profile</title>
+</svelte:head>
+<div id="background" />
+<p>Welcome, I'm Efim</p>
+<p>I'll do the rest of this website later</p>
+
+<style>
+    #background {
+        position: absolute;
+        width: 100vw;
+        height: 100vh;
+        background-color: #121212;
+        z-index: -20;
+    }
+    p {
+        color: #848484;
+        font-size: 3rem;
+    }
+</style>
