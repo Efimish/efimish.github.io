f71a539d3bce1fc6d5d3328590fc345dd765138b
Efimish <efim.ishenin@gmail.com>
2024-02-24 02:56:39 +0700

    translate title and save selected language

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const translations = {
     en: {
+        title: "I am Efim",
         welcome: "👋 Hi, I am Efim",
         about: "18 y.o student from Russia",
         interest: "Interested in back-end development",
@@ -8,6 +9,7 @@ const translations = {
         copied: "Copied!"
     },
     ru: {
+        title: "Я Ефим",
         welcome: "👋 Привет, я Ефим",
         about: "18 летний студент из России",
         interest: "Интересуюсь back-end разработкой",
@@ -17,15 +19,27 @@ const translations = {
     }
 };
 const updateTranslations = (language) => {
+    document.title = translations[language].title;
     for (const i in translations[language]) {
         const element = document.getElementById(i);
         if (!element) continue;
         element.innerText = translations[language][i];
     }
 }
+const getLanguage = () => {
+    let language = localStorage.getItem('language');
+    if (language) return language;
+    language = ['ru', 'ru-RU'].includes(navigator.language) ? 'ru' : 'en';
+    return language;
+}
+const setLanguage = (language) => {
+    if (!Object.keys(translations).includes(language)) throw new Error('Unknown language');
+    localStorage.setItem('language', language);
+    updateTranslations(language);
+}
 
-
-const language = ['ru', 'ru-RU'].includes(navigator.language) ? 'ru' : 'en';
+const language = getLanguage();
+updateTranslations(language);
 
 const languageSelector = document.getElementById('language');
 for (const i in translations) {
@@ -37,10 +51,8 @@ for (const i in translations) {
     }
     languageSelector.appendChild(option);
 }
-updateTranslations(language);
-languageSelector.onchange = () => {
-    updateTranslations(languageSelector.value);
-}
+
+languageSelector.onchange = () => setLanguage(languageSelector.value);
 
 const discord = document.getElementById('discord');
 const discordTooltip = discord.querySelector('p');
