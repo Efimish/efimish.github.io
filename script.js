499e51eae9f207a11a56d52be30a290e40a7134c
Efimish <efim.ishenin@gmail.com>
2024-02-24 00:55:39 +0700

    changed contacts, moved stuff around

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-document.getElementById('discord').onclick = () => navigator.clipboard.writeText('@efima');
+const discord = document.getElementById('discord');
+const discordTooltip = discord.querySelector('p');
+discord.onclick = () => {
+    navigator.clipboard.writeText('efima');
+    const text = discordTooltip.textContent;
+    discordTooltip.textContent = 'Copied!';
+    setTimeout(() => {
+        discordTooltip.textContent = text;
+    }, 1000);
+}
