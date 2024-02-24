8bc46fde39940b9cfb93f3891cc2320f2891c86d
Efimish <efim.ishenin@gmail.com>
2024-02-24 21:56:58 +0700

    fixed script and made it shorter

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 const translations = {
     en: {
-        title: "I am Efim",
-        welcome: "👋 Hi, I am Efim",
-        about: "18 y.o student from Russia",
-        interest: "Interested in back-end development",
-        learning: "Technologies I am learning:",
-        projects: "Some of my projects:",
-        copied: "Copied!"
+        title: 'I am Efim',
+        welcome: '👋 Hi, I am Efim',
+        about: '18 y.o student from Russia',
+        interest: 'Interested in back-end development',
+        learning: 'Technologies I am learning:',
+        projects: 'Some of my projects:',
+        copied: 'Copied!'
     },
     ru: {
-        title: "Я Ефим",
-        welcome: "👋 Привет, я Ефим",
-        about: "18 летний студент из России",
-        interest: "Интересуюсь back-end разработкой",
-        learning: "Технологии, которые я изучаю:",
-        projects: "Некоторые из моих проектов:",
-        copied: "Скопировано!"
+        title: 'Я Ефим',
+        welcome: '👋 Привет, я Ефим',
+        about: '18 летний студент из России',
+        interest: 'Интересуюсь back-end разработкой',
+        learning: 'Технологии, которые я изучаю:',
+        projects: 'Некоторые из моих проектов:',
+        copied: 'Скопировано!'
     }
 };
-const updateTranslations = (language) => {
+const updateTranslations = () => {
+    const language = getLanguage();
     document.title = translations[language].title;
     for (const i in translations[language]) {
         const element = document.getElementById(i);
@@ -26,29 +27,22 @@ const updateTranslations = (language) => {
         element.innerText = translations[language][i];
     }
 }
-const getLanguage = () => {
-    let language = localStorage.getItem('language');
-    if (language) return language;
-    language = ['ru', 'ru-RU'].includes(navigator.language) ? 'ru' : 'en';
-    return language;
-}
+const getLanguage = () => localStorage.getItem('language') || (['ru', 'ru-RU'].includes(navigator.language) ? 'ru' : 'en');
 const setLanguage = (language) => {
     if (!Object.keys(translations).includes(language)) throw new Error('Unknown language');
     localStorage.setItem('language', language);
     updateTranslations(language);
 }
 
-const language = getLanguage();
-updateTranslations(language);
+setLanguage(getLanguage());
+console.log(getLanguage());
 
 const languageSelector = document.getElementById('language');
 for (const i in translations) {
     const option = document.createElement('option');
     option.value = i;
     option.innerText = i;
-    if (i === language) {
-        option.selected = true;
-    }
+    if (i === getLanguage()) option.selected = true;
     languageSelector.appendChild(option);
 }
 
@@ -58,9 +52,8 @@ const discord = document.getElementById('discord');
 const discordTooltip = discord.querySelector('p');
 discord.onclick = () => {
     navigator.clipboard.writeText('efima');
-    const text = discordTooltip.textContent;
-    discordTooltip.textContent = translations[language].copied;
+    discordTooltip.textContent = translations[getLanguage()].copied;
     setTimeout(() => {
-        discordTooltip.textContent = text;
+        discordTooltip.textContent = '@efima';
     }, 1000);
 }
