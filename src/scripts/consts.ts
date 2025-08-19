e771a6fd55db7bd196956fe70fd14e06615963c5
Efim Ishenin <me@efima.ru>
2025-08-19 20:08:05 +0700

    added email link to footer

--- src/scripts/consts.ts
+++ src/scripts/consts.ts
@@ -1,3 +1,4 @@
 export const author = "Efim Ishenin";
+export const email = "me@efima.ru";
 export const github = "https://github.com/Efimish";
 export const linkedin = "https://www.linkedin.com/in/efima";
