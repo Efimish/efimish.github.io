c4a12069dc1d3d38881cc79f54dff29ed7e26dd8
Efim Ishenin <me@efima.ru>
2025-12-20 22:04:16 +0700

    simplify tags rendering, remove word count

--- src/scripts/words.ts
+++
@@ -1,4 +0,0 @@
-import { fromMarkdown } from "mdast-util-from-markdown";
-import { toString } from "mdast-util-to-string";
-
-export default (markdown: string) => toString(fromMarkdown(markdown)).split(" ").length;
