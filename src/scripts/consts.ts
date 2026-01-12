fd405336855345b0a106585436242f07e642dbd1
Efim Ishenin <me@efima.ru>
2026-01-13 02:21:41 +0700

    add json-ld schemas: WebSite, Person, ProfilePage, Blog - add many more constants - import schema-dts for types from schema.org - remove unnecessary blank lines between imports - rename / page to Home - rename website to Efim's corner - add slots back to BaseHead

--- src/scripts/consts.ts
+++ src/scripts/consts.ts
@@ -1,4 +1,12 @@
-export const author = "Efim Ishenin";
+export const firstname = "Efim";
+export const lastname = "Ishenin";
+export const username = "Efimish";
+export const gender = "Male";
+export const pronouns = "he/him";
+
+export const sitename = `${firstname}'s corner`;
+export const author = `${firstname} ${lastname}`;
+
 export const email = "me@efima.ru";
 export const github = "https://github.com/Efimish";
 export const linkedin = "https://www.linkedin.com/in/efima";
