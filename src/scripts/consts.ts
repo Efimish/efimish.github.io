004532f639684ad1efd1fdf14fa7f129ac707a95
Efim Ishenin <me@efima.ru>
2026-01-13 21:43:01 +0700

    implement pwa - add blog name to consts (unused) - add theme colors to consts - add color-scheme meta tag to head - add 2 theme-color meta tags to head - rename app.webmanifest to manifest.json - add display, theme_color, background_color to manifest - change short_name in manifest to make it more unique - move many css properties from body to :root - improve theme.ts script, responsible for changing themes. add ThemeColorManager class that manages theme-color meta tags

--- src/scripts/consts.ts
+++ src/scripts/consts.ts
@@ -4,9 +4,25 @@ export const username = "Efimish";
 export const gender = "Male";
 export const pronouns = "he/him";
 
-export const sitename = `${firstname}'s corner`;
 export const author = `${firstname} ${lastname}`;
+export const sitename = `${firstname}'s corner`;
+export const blogname = `${author}'s Blog`;
 
 export const email = "me@efima.ru";
 export const github = "https://github.com/Efimish";
 export const linkedin = "https://www.linkedin.com/in/efima";
+
+export const themes = {
+  light: {
+    bgPrimary: "#f0f0f0",
+    bgSecondary: "#e0e0e0",
+    textPrimary: "#101010",
+    textAccent: "#007aff",
+  },
+  dark: {
+    bgPrimary: "#101010",
+    bgSecondary: "#282828",
+    textPrimary: "#f0f0f0",
+    textAccent: "#0a84ff",
+  },
+} as const;
--- src/scripts/theme.ts
+++ src/scripts/theme.ts
@@ -1,5 +1,49 @@
-const theme = localStorage.getItem("theme") ?? "auto";
-document.documentElement.dataset.theme = theme;
+import { themes } from "@scripts/consts";
+
+type Theme = keyof typeof themes | "auto";
+
+class ThemeColorManager {
+  private metaTags: HTMLMetaElement[];
+  private initialMetaTagsValues: string[];
+
+  constructor() {
+    this.metaTags = Array.from(
+      document.querySelectorAll<HTMLMetaElement>('meta[name="theme-color"]')
+    );
+    this.initialMetaTagsValues = this.metaTags.map((meta) => meta.content);
+  }
+
+  setThemeColor(color: string) {
+    this.metaTags.forEach((meta) => {
+      meta.content = color;
+    });
+  }
+
+  resetThemeColor() {
+    this.metaTags.forEach((meta, i) => {
+      meta.content = this.initialMetaTagsValues[i];
+    });
+  }
+
+  loadTheme() {
+    const theme = (localStorage.getItem("theme") ?? "auto") as Theme;
+    this.applyTheme(theme);
+    return theme;
+  }
+
+  applyTheme(theme: Theme) {
+    localStorage.setItem("theme", theme);
+    document.documentElement.dataset.theme = theme;
+    if (theme === "auto") {
+      this.resetThemeColor();
+    } else {
+      this.setThemeColor(themes[theme].bgPrimary);
+    }
+  }
+}
+
+const themeColorManager = new ThemeColorManager();
+const theme = themeColorManager.loadTheme();
 
 const selector = document.querySelector<HTMLSelectElement>("#theme-selector");
 
@@ -7,8 +51,6 @@ if (selector) {
   selector.value = theme;
 
   selector.addEventListener("change", () => {
-    const value = selector.value;
-    localStorage.setItem("theme", value);
-    document.documentElement.dataset.theme = value;
+    themeColorManager.applyTheme(selector.value as Theme);
   });
 }
