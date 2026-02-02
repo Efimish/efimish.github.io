60acab5f6624888e9d004e46110b97233c135154
Efim Ishenin <me@efima.ru>
2026-02-03 02:55:53 +0700

    replace markdown-it with astro's built in parser

--- src/pages/feed.xml.ts
+++ src/pages/feed.xml.ts
@@ -2,10 +2,7 @@ import type { APIRoute } from "astro";
 import { getCollection } from "astro:content";
 import rss from "@astrojs/rss";
 import sanitize from "sanitize-html";
-import MarkdownIt from "markdown-it";
 import { blogname, blogdesc } from "@scripts/consts";
-
-const parser = new MarkdownIt();
 
 export const GET: APIRoute = async (context) => {
   const posts = (await getCollection("posts")).sort(
@@ -22,7 +19,15 @@ export const GET: APIRoute = async (context) => {
       pubDate: post.data.date,
       categories: post.data.tags,
       link: `/posts/${post.id}/`,
-      content: sanitize(parser.render(post.body ?? "")),
+      content: sanitize(post.rendered?.html ?? "", {
+        allowedTags: sanitize.defaults.allowedTags
+          .concat(["del", "input"])
+          .filter(tag => tag !== "span"),
+        allowedAttributes: {
+          ...sanitize.defaults.allowedAttributes,
+          input: ["type", "checked", "disabled"],
+        },
+      }),
     })),
   });
 };
