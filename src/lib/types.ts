b2ce35b49a0d6ad36e3b1e5ac9727bad43361cc9
Efim Ishenin <me@efima.ru>
2026-04-04 20:03:07 +0700

    refactor: open graph data is now passed to BaseHead as a prop

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -19,3 +19,11 @@ export type Schema =
   | BlogPosting;
 
 export type SchemaWithContext = WithContext<Schema>;
+
+export interface Metadata {
+  title: string;
+  description: string;
+  image?: string;
+  og?: { "og:type": string } & Record<string, string | string[] | undefined>;
+  schemas?: Schema[];
+};
